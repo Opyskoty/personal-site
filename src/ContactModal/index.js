e527a53479a811f3c3e2ee5f01c60910ff3b6cf7
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-08-13 10:59:52 -0400

    add modal for contact info

---
+++ src/ContactModal/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import "./ContactModal.css"
+
+function ContactModal({ toggle, modal, className }) {
+  const emailLink = "mailto:olivia.pyskoty@gmail.com";
+  const linkedInLink = "https://www.linkedin.com/in/olivia-pyskoty-5591b2104/";
+  const gitHubLink = "https://github.com/Opyskoty";
+  return (
+    <div className="ContactModal">
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle} className="contactLink">Contact Me</ModalHeader>
+        <ModalBody className="text-center">
+          <p>
+            <a className="contactLink" href={emailLink}>Email</a>
+          </p>
+          <p>
+            <a className="contactLink" href={linkedInLink} target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            <a className="contactLink" href={gitHubLink} target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </p>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="link" onClick={toggle} className="contactLink">
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default ContactModal;
