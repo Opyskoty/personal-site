eb83dc26862f138074c8b61b8fb9c0843ff68702
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-08-16 12:45:19 -0400

    abstract carousel items and georgie pics away from component

--- src/Georgie/index.js
+++ src/Georgie/index.js
@@ -9,44 +9,7 @@ import {
   CarouselControl,
   CarouselIndicators
 } from "reactstrap";
-import georgie1 from "./georgie1.jpg";
-import georgie2 from "./georgie2.png";
-import georgie3 from "./georgie3.JPG";
-import georgie4 from "./georgie4.JPG";
-import georgie5 from "./georgie5.jpg";
-import georgie6 from "./georgie6.jpg";
-import georgie7 from "./georgie7.JPG";
-
-const items = [
-  {
-    src: georgie1,
-    altText: "Georgie 1",
-  },
-  {
-    src: georgie2,
-    altText: "Georgie 2",
-  },
-  {
-    src: georgie3,
-    altText: "Georgie 3",
-  },
-  {
-    src: georgie4,
-    altText: "Georgie 4",
-  },
-  {
-    src: georgie5,
-    altText: "Georgie 5",
-  },
-  {
-    src: georgie6,
-    altText: "Georgie 6",
-  },
-  {
-    src: georgie7,
-    altText: "Georgie 7",
-  },
-];
+import items from "./items"
 
 function Georgie() {
   const [activeIndex, setActiveIndex] = useState(0);
